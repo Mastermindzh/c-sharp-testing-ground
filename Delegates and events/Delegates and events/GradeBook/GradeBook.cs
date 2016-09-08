08ae843147797eaef569be557614bdc516b9112d
mastermindzh <Rick@mi-soft.nl>
2016-09-08 14:35:24 +0200

    Delegates and events

---
+++ Delegates and events/Delegates and events/GradeBook/GradeBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_and_events.GradeBook
+{
+	public class GradeBook
+	{
+		string name = "Empty";
+		List<float> grades = new List<float>();
+		public event NameChangedDelegate NameChanged;
+
+
+		public void addGrade(float f)
+		{
+			grades.Add(f);
+		}
+
+		public string Name
+		{
+			get {
+				return name;
+			}
+			set {
+				if (!String.IsNullOrEmpty(value))
+				{
+					if(name != value)
+					{
+						NameChanged(this,new NameChangedEventArgs { ExistingName = name, NewName = value});
+					}
+					name = value;
+				}
+			}
+		}
+		
+	}
+}
---
+++ Delegates and events/Delegates and events/Program.cs
@@ -0,0 +1,58 @@
+﻿using Delegates_and_events.GradeBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_and_events
+{
+	class Program
+	{
+		public delegate void Writer(string message);
+
+		static void Main()
+		{
+			#pragma warning disable RECS0026 // Possible unassigned object created by 'new'
+			new Program();
+			#pragma warning restore RECS0026 // Possible unassigned object created by 'new'
+		}
+
+		public Program() {
+			var logger = new Logger();
+			var writer = new Writer(logger.WriteMessage);
+			#pragma warning disable RECS0021 // Warns about calls to virtual member functions occuring in the constructor
+			writer("Succes");
+			#pragma warning restore RECS0021 // Warns about calls to virtual member functions occuring in the constructor
+
+			
+			GradeBook();
+
+			Console.ReadKey();
+		}
+
+		void GradeBook() {
+			var book = new GradeBook.GradeBook();
+
+			// using += to make sure we SUBSCRIBE to the delegate and not overwrite it :)
+			// we can remove a method with -=
+			#pragma warning disable RECS0164 // Explicit delegate creation expression is redundant
+			book.NameChanged += new NameChangedDelegate(OnNameChanged);
+			#pragma warning restore RECS0164 // Explicit delegate creation expression is redundant
+
+			// we don't need new NameChangedDelegate
+			book.NameChanged += OnNameChanged;
+
+			book.Name = "testing";
+			
+		}
+
+		void OnNameChanged(Object o, NameChangedEventArgs args)
+		{
+			Console.WriteLine("Grade book changing from {0} to {1}", args.ExistingName, args.NewName);
+		}
+
+
+
+	}
+}
