e72e91d8b7afa2dd723caee52a4c912b2527d216
Rick van Lieshout <lieshori@inforit.local>
2016-09-08 13:23:37 +0200

    data driven tests

---
+++ TestingTestingGround/Calculator/Classes/MemoryCalculator.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculatorTest.Classes
+{
+	public class MemoryCalculator
+	{
+		public int Amount { get; set; }
+
+		public void Substract(int value)
+		{
+			Amount -= value;
+		}
+	}
+}
---
+++ TestingTestingGround/CalculatorTest/Classes/MemoryCalculatorDataDrivenTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorTest.Classes
+{
+	[TestFixture]
+	public class MemoryCalculatorDataDrivenTests
+	{
+		[TestCase(5,10,-15)]
+		[TestCase(-5, -5, 10)]
+		public void ShouldSubtractTwoNegativeNumbers(int firstNum, int secondNum, int expected)
+		{
+			var sut = new MemoryCalculator();
+
+			sut.Substract(firstNum);
+			sut.Substract(secondNum);
+
+			Assert.That(sut.Amount, Is.EqualTo(expected));
+		}
+		
+	}
+}
