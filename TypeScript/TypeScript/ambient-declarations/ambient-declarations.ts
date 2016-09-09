bc290b1353ad21bbb75643797acef60b57d187f3
mastermindzh <Rick@mi-soft.nl>
2016-09-09 11:27:13 +0200

    ambiant declarations & Typings

---
+++ TypeScript/TypeScript/ambient-declarations/ambient-declarations.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="knockout.d.ts" />
+module ambientDeclarations {
+    "use strict";
+
+
+    declare var ko: KnockoutStatic;
+
+    var name: KnockoutObservable<string> = ko.observable("Rick");
+    var id:  KnockoutObservable<number> = ko.observable(1);
+
+    var guy = {
+        id: id,
+        fullName: name
+    };
+
+    var value: string = guy.fullName();
+    console.log(value);
+
+}
