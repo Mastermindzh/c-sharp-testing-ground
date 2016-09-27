ebc9c6e251ea342a65fc5577a81185923e811eb4
Mastermindzh <Rick@mi-soft.nl>
2016-09-27 10:04:37 +0200

    iso 3166-1 alpha-2 to displayname test

---
+++ c#/regionInfoLookupTest/regionInfoLookupTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace regionInfoLookupTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			// one liner
+			Console.WriteLine(new System.Globalization.RegionInfo("NL").DisplayName);
+
+			Console.WriteLine();
+			Console.WriteLine("=========");
+			Console.WriteLine();
+
+			System.Globalization.RegionInfo region = new System.Globalization.RegionInfo("NL");
+			Console.WriteLine("Name:                         {0}", region.Name);
+			Console.WriteLine("DisplayName:                  {0}", region.DisplayName);
+			Console.WriteLine("EnglishName:                  {0}", region.EnglishName);
+			Console.WriteLine("IsMetric:                     {0}", region.IsMetric);
+			Console.WriteLine("ThreeLetterISORegionName:     {0}", region.ThreeLetterISORegionName);
+			Console.WriteLine("ThreeLetterWindowsRegionName: {0}", region.ThreeLetterWindowsRegionName);
+			Console.WriteLine("TwoLetterISORegionName:       {0}", region.TwoLetterISORegionName);
+			Console.WriteLine("CurrencySymbol:               {0}", region.CurrencySymbol);
+			Console.WriteLine("ISOCurrencySymbol:            {0}", region.ISOCurrencySymbol);
+			Console.ReadLine();
+		}
+	}
+}
