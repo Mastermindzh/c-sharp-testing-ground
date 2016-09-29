1cbf1c27de27b2ef35016ddfae2b8001248a07e3
Mastermindzh <Rick@mi-soft.nl>
2016-09-29 13:16:06 +0200

    windows desktop notification tray

---
+++ c#/Desktop notifications-tray/Desktop notifications-tray/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Desktop_notifications_tray
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.item1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.item2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.item3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Notification test";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.NotifyIcon1_Click);
+			this.notifyIcon1.Click += new System.EventHandler(this.NotifyIcon1_Click);
+			this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.item1ToolStripMenuItem,
+            this.item2ToolStripMenuItem,
+            this.item3ToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(115, 70);
+			// 
+			// item1ToolStripMenuItem
+			// 
+			this.item1ToolStripMenuItem.Name = "item1ToolStripMenuItem";
+			this.item1ToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+			this.item1ToolStripMenuItem.Text = "Item #1";
+			// 
+			// item2ToolStripMenuItem
+			// 
+			this.item2ToolStripMenuItem.Name = "item2ToolStripMenuItem";
+			this.item2ToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+			this.item2ToolStripMenuItem.Text = "Item #2";
+			// 
+			// item3ToolStripMenuItem
+			// 
+			this.item3ToolStripMenuItem.Name = "item3ToolStripMenuItem";
+			this.item3ToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+			this.item3ToolStripMenuItem.Text = "Item #3";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(37, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Status";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 13);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(13, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "0";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(174, 36);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(261, 71);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Notification tester";
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem item1ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem item2ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem item3ToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ c#/Desktop notifications-tray/Desktop notifications-tray/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Desktop_notifications_tray
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			notifyIcon1.BalloonTipText = "Windows notification";
+			notifyIcon1.ShowBalloonTip(100);
+			notifyIcon1.ShowBalloonTip(100, "title", "text", new ToolTipIcon());
+		}
+
+		private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			label2.Text = "doubleclick";
+		}
+
+		private void NotifyIcon1_Click(object sender, EventArgs e)
+		{
+			label2.Text = "Clicked the balloon!";
+		}
+
+
+	}
+}
